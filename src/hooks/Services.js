04f69a8f1e5b05379cf5c6f77e07faf11f9250ab
ejferte <ejferte@gmail.com>
2023-03-21 11:20:40 -0700

    setup required API calls in Hooks/Service.js

---
+++ src/hooks/Services.js
@@ -0,0 +1,210 @@
+import { useEffect, useState } from 'react'
+import APIKEYDATA from '../apikey.json'
+const APIKEY = APIKEYDATA.key
+
+function GetSummonerID(query) {
+    const [ summoner, setSummoner ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(false)
+
+    useEffect(() => {
+        let ignore = false
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            setLoading(true)
+            let responseBody = {}
+            try {
+                const response = await fetch(
+                    `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${query}?api_key=${APIKEY}`,
+                    { signal: controller.signal }
+                )
+                if (response.status !== 200) {
+                    console.log("== status:", response.status)
+                    setError(true)
+                } else {
+                    setError(false)
+                    responseBody = await response.json()
+                }
+            } catch (e) {
+                if (e instanceof DOMException) {
+                    console.log("HTTP request cancelled")
+                } else {
+                    setError(true)
+                    console.error("Error:", e)
+                    throw e
+                }
+            }
+
+            if (!ignore) {
+                setSummoner(responseBody.id || {})
+                setLoading(false)
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => {
+            ignore = true
+            controller.abort()
+        }
+    }, [ query ])
+
+    return [ summoner, loading, error ]
+}
+
+//takes the encryptedSummonerId from the query above to pull champion mastery list for that player
+//sorted by number of champion points descending
+function GetChampionList(query) {
+    const [ champion, setChampion ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(false)
+
+    useEffect(() => {
+        let ignore = false
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            setLoading(true)
+            let responseBody = {}
+            try {
+                const response = await fetch(
+                    `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${query}?api_key=${APIKEY}`,
+                    { signal: controller.signal }
+                )
+                if (response.status !== 200) {
+                    console.log("== status:", response.status)
+                    setError(true)
+                } else {
+                    setError(false)
+                    responseBody = await response.json()
+                }
+            } catch (e) {
+                if (e instanceof DOMException) {
+                    console.log("HTTP request cancelled")
+                } else {
+                    setError(true)
+                    console.error("Error:", e)
+                    throw e
+                }
+            }
+
+            if (!ignore) {
+                console.log("** Response: ", responseBody)
+                setChampion(responseBody.championId || {})
+                setLoading(false)
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => {
+            ignore = true
+            controller.abort()
+        }
+    }, [ query ])
+
+    return [ champion, loading, error ]
+}
+
+//takes the puuid from the GetSummonerID function above to request the 10 most recent matchId's for that player
+function GetMatchHistory(query) {
+    const [ matchList, setMatchList ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(false)
+
+    useEffect(() => {
+        let ignore = false
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            setLoading(true)
+            let responseBody = {}
+            try {
+                const response = await fetch(
+                    `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${query}/ids?start=0&count=10&api_key=${APIKEY}`,
+                    { signal: controller.signal }
+                )
+                if (response.status !== 200) {
+                    console.log("== status:", response.status)
+                    setError(true)
+                } else {
+                    setError(false)
+                    responseBody = await response.json()
+                }
+            } catch (e) {
+                if (e instanceof DOMException) {
+                    console.log("HTTP request cancelled")
+                } else {
+                    setError(true)
+                    console.error("Error:", e)
+                    throw e
+                }
+            }
+
+            if (!ignore) {
+                console.log("** Response: ", responseBody)
+                setMatchList(responseBody || {})
+                setLoading(false)
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => {
+            ignore = true
+            controller.abort()
+        }
+    }, [ query ])
+
+    return [ matchList, loading, error ]
+}
+
+//gets ALL match details for the given MatchID, to find details for our specific player we must search through the Participant list using our summoners puuid
+function GetMatchHistory(query) {
+    const [ matchDetail, setMatchDetail ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(false)
+
+    useEffect(() => {
+        let ignore = false
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            setLoading(true)
+            let responseBody = {}
+            try {
+                const response = await fetch(
+                    `https://americas.api.riotgames.com/lol/match/v5/matches/${query}?api_key=${APIKEY}`,
+                    { signal: controller.signal }
+                )
+                if (response.status !== 200) {
+                    console.log("== status:", response.status)
+                    setError(true)
+                } else {
+                    setError(false)
+                    responseBody = await response.json()
+                }
+            } catch (e) {
+                if (e instanceof DOMException) {
+                    console.log("HTTP request cancelled")
+                } else {
+                    setError(true)
+                    console.error("Error:", e)
+                    throw e
+                }
+            }
+
+            if (!ignore) {
+                console.log("** Response: ", responseBody)
+                setMatchDetail(responseBody || {})
+                setLoading(false)
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => {
+            ignore = true
+            controller.abort()
+        }
+    }, [ query ])
+
+    return [ matchDetail, loading, error ]
+}
