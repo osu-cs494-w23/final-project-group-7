2df54c71c68b1e0a140d1486ffdf0d9b88f3beb9
Chelsea Li <chelseali2001@gmail.com>
2023-03-20 21:58:05 -0700

    You can delete a player or clear all of the players from the deck

--- src/pages/summonerComp.js
+++ src/pages/summonerComp.js
@@ -17,9 +17,22 @@ export default function SummonerComp() {
         setSearchInput("");
     };
 
+    const handleClearAll = () => {
+        setPlayers([])
+    }
+
+    const handleDelete = (playerName) => {
+        setPlayers((current) =>
+            current.filter(
+                (player) =>
+                    player != playerName
+            )
+        )
+    };
+
     return (
         <>
-            <h1>Account/Summoner Comparisons</h1>
+            <h1 className='summoner-comparison-title'>Account/Summoner Comparisons</h1>
             <input 
                 className='search-bar' 
                 type="text" 
@@ -30,11 +43,20 @@ export default function SummonerComp() {
 
             <button onClick={handleSubmit}>Search</button>
 
-            <div className='players-container'>
-                {players.map((player, index) => 
-                    <div className='player-stats' key={index}>{player.searchInput}</div>
-                )}
-            </div>
+            {players.length === 0 ? 
+            (<></>) : (
+                <div>
+                    <button className='clear-all' onClick={() => handleClearAll()}>Clear All Summoners</button>
+                    <div className='players-container'>
+                        {players.map((player, index) => 
+                            <div className='player-stats' key={index}>
+                                <button className='delete-player' onClick={() => handleDelete(player)}>X</button>
+                                <div>{player.searchInput}</div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
         </>
     )
 }
