57b415e54530a221e2ecfaf8caa0c240bb822e55
Chelsea Li <chelseali2001@gmail.com>
2023-03-20 21:33:15 -0700

    You can now look up a player and have it be displayed to the screen (no real data being used yet)

--- src/pages/summonerComp.js
+++ src/pages/summonerComp.js
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import './summonerComp.css';
+
 export default function SummonerComp() {
-    return(
+    const [searchInput, setSearchInput] = useState("");
+    const [players, setPlayers] = useState([]);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newList = players.concat({searchInput});
+        setPlayers(newList);
+        setSearchInput("");
+    };
+
+    return (
         <>
-            <h1>Summoner Comparisons</h1>
+            <h1>Account/Summoner Comparisons</h1>
+            <input 
+                className='search-bar' 
+                type="text" 
+                name="player-name" 
+                placeholder='Enter Account/Summoner Name' 
+                onChange={handleChange}
+                value={searchInput} />
+
+            <button onClick={handleSubmit}>Search</button>
+
+            <div className='players-container'>
+                {players.map((player, index) => 
+                    <div className='player-stats' key={index}>{player.searchInput}</div>
+                )}
+            </div>
         </>
     )
 }
