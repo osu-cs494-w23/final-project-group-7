659c9b30ffbc5980dce3a90090f7e62caa18c9f9
Chelsea Li <chelseali2001@gmail.com>
2023-03-22 15:26:25 -0700

    Took out game name and tagline entry

--- src/hooks/Services.js
+++ src/hooks/Services.js
@@ -261,3 +261,53 @@ export function GetSummonerStats(query) {
 
     return [ stats, loading, error ]
 }
+
+export function GetAccount(query) {
+    const [ account, setAccount ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(false)
+
+    useEffect(() => {
+        let ignore = false
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            setLoading(true)
+            let responseBody = {}
+            try {
+                const response = await fetch(
+                    `https://na1.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${query}?api_key=${APIKEY}`,
+                    { signal: controller.signal }
+                )
+                if (response.status !== 200) {
+                    console.log("== status:", response.status)
+                    setError(true)
+                } else {
+                    setError(false)
+                    responseBody = await response.json()
+                }
+            } catch (e) {
+                if (e instanceof DOMException) {
+                    console.log("HTTP request cancelled")
+                } else {
+                    setError(true)
+                    console.error("Error:", e)
+                    throw e
+                }
+            }
+
+            if (!ignore) {
+                setAccount(responseBody || {})
+                setLoading(false)
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => {
+            ignore = true
+            controller.abort()
+        }
+    }, [ query ])
+
+    return [ account, loading, error ]
+}
--- src/pages/summonerComp.js
+++ src/pages/summonerComp.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect } from 'react'
+import {React, useState } from 'react'
 import './summonerComp.css';
 import GetSummonerID from '../hooks/Services'
 import { GetSummonerStats, GetMatchHistory, GetMatchDetails } from '../hooks/Services'
@@ -25,22 +25,12 @@ function MatchDetails(props) {
   }
 
 function SummonerCard() {
-    const [selectedSearch, setSelectSearch] = useState("name");
     const [searchInput, setSearchInput] = useState("");
     const [value, setValue] = useState("");
-    const [gameValue, setGameValue] = useState("");
-    const [tagValue, setTagValue] = useState("");
     const [summoner, loading, error] = GetSummonerID(searchInput);
     const [stats] = GetSummonerStats(summoner.id);
     const [matchList] = GetMatchHistory(summoner.puuid);
     const [selectedMatch, setSelectMatch] = useState(0);
-
-    const handleSelect = (e) => {
-        setSelectSearch(e.target.value);
-        setValue("");
-        setGameValue("");
-        setGameValue("");
-    }
 
     const handleSelectMatch = (e) => {
         setSelectMatch(e.target.value);
@@ -48,34 +38,14 @@ function SummonerCard() {
 
     return (
         <div>
-            <select value={selectedSearch} onChange={handleSelect}>
-                <option value="name">Search by name</option>
-                <option value="game_tag">Search by game name and tag line</option>
-            </select>
-
-            {selectedSearch === "name" ? (
-                <>
-                    <form onSubmit = {e => {
-                        e.preventDefault()
-                        setSearchInput(value)
-                        setValue("")
-                    }}>
-                        <input value={value} onChange={e => setValue(e.target.value)} placeholder="Enter Summoner Name"/>
-                        <button type="submit">Search</button>
-                    </form>
-                </>
-                ) : (
-                    <form onSubmit = {e => {
-                        e.preventDefault()
-                        setSearchInput(gameValue + "/" + tagValue)
-                        setGameValue("")
-                        setTagValue("")
-                    }}>
-                        <input value={gameValue} onChange={e => setGameValue(e.target.value)} placeholder="Enter Game Name"/>
-                        <input value={tagValue} onChange={e => setTagValue(e.target.value)} placeholder="Enter Tag Line"/>
-                        <button type="submit">Search</button>
-                    </form>
-            )}
+            <form onSubmit = {e => {
+                e.preventDefault()
+                setSearchInput(value)
+                setValue("")
+            }}>
+                <input value={value} onChange={e => setValue(e.target.value)} placeholder="Enter Summoner Name"/>
+                <button type="submit">Search</button>
+            </form>
 
             {!loading && !error && 
                 <div className='summoner-stats-container'>
@@ -103,7 +73,7 @@ function SummonerCard() {
                     }
                 </div>
             }
-            {error && loading && <h1>Failed to Find Summoner!</h1>}
+            {error && loading && <h1>Failed to Find Summoner, {searchInput}!</h1>}
             {loading && !error && <h1>Loading...</h1>}
         </div>
     )
